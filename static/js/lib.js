ec291156e9e24b0bc2dc479a6f5f400f9eb191a4
NokonoKotlin <lr@rinoMac-mini.local>
2024-10-11 01:33:49 +0900

    first commit (完成)

---
+++ static/js/lib.js
@@ -0,0 +1,36 @@
+const base = "9+9-8-2-4-4+3-5+3";
+const base_double = base + "+" + base;
+
+// S to (S)
+function format(S){return "(" + S + ")";}
+
+// N to 998244353;
+function convert998(N){
+    console.log(N);
+    if((typeof N) != 'number')return "invalid";
+    var toggle = false;
+
+    if( N == 0 )return base + "-" + base;
+    if(N < 0){
+        toggle = true;
+        N = N*(-1);
+    }
+    var power2 = format(base);
+    var res = "";
+    for(var bit = 0; bit < 30 ; bit++){
+        if(N&(1<<bit)){
+            if(res.length == 0)res = power2;
+            else res = res + "+" + power2;
+        }
+        power2 = format(power2 + "*" + format(base_double));
+    }
+    if(toggle) res = "-" + format(res);
+    return res;
+}
+
+
+function convert_and_display(){
+    var inp = document.getElementById("mainform").value;
+    var display_area = document.getElementById("display_area");
+    display_area.innerText = inp + " = " + convert998(parseInt(inp));
+}
